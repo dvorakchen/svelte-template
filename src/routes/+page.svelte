1b6dcc94cd639f5c12f879285c0fe4bf6e6193db
DvorakChen <dvorakchen@outlook.com>
2025-08-26 20:58:29 +0800

    style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 </script>
 
-<p class="p-4">
+<p>
 	<button class="btn">Default</button>
 	<button class="btn btn-primary">
 		<span class="icon-[mdi--home]"></span>
@@ -15,7 +15,7 @@
 	<button class="btn btn-info">Info</button>
 	<button class="btn btn-success">Success</button>
 </p>
-<p class="p-4">
+<p>
 	<button class="btn btn-outline">Default</button>
 	<button class="btn btn-primary btn-outline">Default</button>
 	<button class="btn btn-outline btn-secondary">Secondary</button>
@@ -26,3 +26,59 @@
 	<button class="btn btn-outline btn-info">Info</button>
 	<button class="btn btn-outline btn-success">Success</button>
 </p>
+
+<div class="p-1">
+	<div class="dropdown">
+		<div tabindex="0" role="button" class="btn m-1">Dropdown 1</div>
+		<ul tabindex="0" class="dropdown-content menu z-1 w-52 rounded-box bg-base-100 p-2 shadow-sm">
+			<li><a>Item 1</a></li>
+			<li><a>Item 2</a></li>
+		</ul>
+	</div>
+
+	<div class="dropdown">
+		<div tabindex="0" role="button" class="btn m-1">Dropdown 2</div>
+		<ul tabindex="0" class="dropdown-content menu z-1 w-52 rounded-box bg-base-200 p-2 shadow-sm">
+			<li><a>Item 1</a></li>
+			<li><a>Item 2</a></li>
+		</ul>
+	</div>
+
+	<div class="dropdown">
+		<div tabindex="0" role="button" class="btn m-1">Dropdown 3</div>
+		<ul tabindex="0" class="dropdown-content menu z-1 w-52 rounded-box bg-base-300 p-2 shadow-sm">
+			<li><a>Item 1</a></li>
+			<li><a>Item 2</a></li>
+		</ul>
+	</div>
+</div>
+
+<p>
+	<input type="text" class="input" placeholder="input" />
+</p>
+
+<p>
+	<input type="checkbox" checked class="toggle toggle-primary" />
+	<input type="checkbox" checked class="toggle toggle-secondary" />
+	<input type="checkbox" checked class="toggle toggle-accent" />
+	<input type="checkbox" checked class="toggle toggle-neutral" />
+	<input type="checkbox" checked class="toggle toggle-info" />
+	<input type="checkbox" checked class="toggle toggle-success" />
+	<input type="checkbox" checked class="toggle toggle-warning" />
+	<input type="checkbox" checked class="toggle toggle-error" />
+</p>
+
+<p>
+	<input type="radio" name="radio-1" class="radio" checked />
+	<input type="radio" name="radio-1" class="radio" />
+	<input type="radio" name="radio-4" class="radio radio-primary" checked />
+	<input type="radio" name="radio-4" class="radio radio-primary" />
+	<input type="radio" name="radio-5" class="radio radio-secondary" checked />
+	<input type="radio" name="radio-5" class="radio radio-secondary" />
+</p>
+
+<style>
+	p {
+		padding: 1rem;
+	}
+</style>
